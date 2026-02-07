2c80df5ae69fbc7c1009cfdadabd0ce46beadad1
K4U4ZINHO <kaique.e.kaua.freefire@gmail.com>
2026-02-07 15:17:18 +0000

    rodando no meu app

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5500, debug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
 
 
     # para matar o processo da base de dados
